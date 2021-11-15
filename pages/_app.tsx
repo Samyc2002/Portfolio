11ee2cf3dd21d8a4d52a35396a53f78d7e4b9ee8
“Samriddha <200020040@iitdh.ac.in>
2021-11-15 14:10:38 +0530

    Boilerplate added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,8 @@
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-import '../styles/globals.css'
-import ColourScheme from '../contexts/ColourScheme';
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ColourScheme>
-      <Component {...pageProps} />
-    </ColourScheme>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
