c59b19be8b471912ecdaff9e6a314ac972a1a901
“Samriddha <200020040@iitdh.ac.in>
2021-11-17 00:25:56 +0530

    Added About section. Gotta work on responsiveness next lol.

--- components/index.tsx
+++ components/index.tsx
@@ -2,3 +2,4 @@ export { default as Header } from './Header';
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Separator } from './Separator';
+export { default as About } from './About';
